19b8830fc498c22d4d4f73955bc5191e50135c71
GracieleSilva <graci_selma@hotmail.com>
2025-07-20 14:39:57 -0300

    'permittedChar'

--- src/services/password/handler.ts
+++ src/services/password/handler.ts
@@ -1,16 +1,7 @@
-const permittedCharacters = async () => {
-    let characters: string[] = [];
-
-    if(process.env.UPPERCASE_LETTERS === "true") characters.push( ... "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    if(process.env.LOWERCASE_LETTERS === "true") characters.push( ... "abcdefghijklmnopqrstuvwxyz");
-    if(process.env.NUMBERS === "true") characters.push( ... "0123456789");
-    if(process.env.SPECIAL_CHARACTERS === "true") characters.push( ... "!@#$%^&*()-_");
-
-    return characters;
-}
+import permittedChar from './utils/permittedCharacters';
 
 const handle = async () => {
-    let characters: string[]  = await permittedCharacters();
+    let characters: string[]  = await permittedChar();
     let password: string = "";
 
     const passwordLength: number = Number(process.env.PASSWORD_LENGHT);
